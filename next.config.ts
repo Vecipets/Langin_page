ad05e58ffb429d8cc26198233dbcbae468274c41
maximilianoosorio <maxinal012208@gmail.com>
2026-06-22 18:32:42 -0500

    configuracion de la landig page

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/Langin_page", // Este es el nombre exacto de tu repo en GitHub
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
